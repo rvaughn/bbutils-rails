6db0584144e422456823c05f3a24f7dae39081c5
Roger Vaughn <rvaughn@pobox.com>
2015-05-01 16:22:53 -0400

    Big styles rework.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,36 @@
 module ApplicationHelper
 
+  # use comment out html code server-side
   def comment
+  end
+
+  def link_with_icon(path, icon_name, text = '')
+    content_tag(:a, text + fa_icon(icon_name), { href: url_for(path) }, false)
+  end
+
+  # icon mappers, for easy swapping
+  def icon_overview
+    "dashboard"
+  end
+
+  def icon_repositories
+    "code-fork"
+  end
+
+  def icon_members
+    "user"
+  end
+
+  def icon_groups
+    "group"
+  end
+
+  def icon_expand_sidebar
+    "angle-double-right"
+  end
+
+  def icon_shrink_sidebar
+    "angle-double-left"
   end
   
 end
